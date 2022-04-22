b64b626b882c730ed82b44ddb1544cbb38c0a569
Michael Aldridge <aldridge.mac@gmail.com>
2022-04-21 23:19:41 -0500

    ports: Configure port for wireless access

--- terraform/network/core/ports.tf
+++ terraform/network/core/ports.tf
@@ -1,3 +1,14 @@
+resource "junos_interface_physical" "wap1" {
+  name = "ge-0/0/3"
+  trunk = true
+  vlan_members = [
+    junos_vlan.vlans["iot"].id,
+    junos_vlan.vlans["residential"].id,
+    junos_vlan.vlans["guest"].id,
+    junos_vlan.vlans["mgmt"].id,
+  ]
+}
+
 resource "junos_interface_physical" "svcs_host1" {
   name         = "ge-0/0/4"
   trunk        = true
