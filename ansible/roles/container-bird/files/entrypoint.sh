72b86bf77c0a87ee91af4dfb3a88ffec9461fabe
Michael Aldridge <aldridge.mac@gmail.com>
2022-06-12 18:28:18 -0500

    ansible: Fix peering routing issues

--- ansible/roles/container-bird/files/entrypoint.sh
+++ ansible/roles/container-bird/files/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 ln -sf /data/bird.conf /etc/bird.conf
-wg-quick up peer0
+ln -sf /data/wg/* /etc/wireguard/
+for tun in /etc/wireguard/*.conf ; do
+    wg-quick up $tun
+done
 exec /usr/bin/tini /usr/bin/runsvdir /var/service
