bf9860bee65e388bebce9a300c31efa064ae3320
Michael Aldridge <aldridge.mac@gmail.com>
2023-04-09 00:09:40 -0500

    terraform/network/core: Deconfigure unused ports

--- terraform/network/core/ports.tf
+++ terraform/network/core/ports.tf
@@ -4,20 +4,3 @@ resource "junos_interface_physical" "core_sw1" {
   vlan_members = [for vlan in junos_vlan.vlans : vlan.id]
   vlan_native  = junos_vlan.vlans["mgmt"].vlan_id
 }
-
-resource "junos_interface_physical" "nas" {
-  name         = "ge-0/0/3"
-  vlan_members = [junos_vlan.vlans["services"].vlan_id]
-}
-
-resource "junos_interface_physical" "svcs_host1" {
-  name         = "ge-0/0/4"
-  trunk        = true
-  vlan_members = [for vlan in junos_vlan.vlans : vlan.id]
-}
-
-resource "junos_interface_physical" "net_svcs_host" {
-  name         = "ge-0/0/5"
-  trunk        = true
-  vlan_members = [for vlan in junos_vlan.vlans : vlan.id]
-}
