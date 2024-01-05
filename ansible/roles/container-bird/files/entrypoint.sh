680d36fca9e080863d9d601d58e00c289dc14fd5
Michael Aldridge <aldridge.mac@gmail.com>
2024-01-04 23:34:30 -0600

    ansible: Remove unused bird container

--- ansible/roles/container-bird/files/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ln -sf /data/bird.conf /etc/bird.conf
-ln -sf /data/wg/* /etc/wireguard/
-for tun in /etc/wireguard/*.conf ; do
-    wg-quick up $tun
-done
-exec /usr/bin/tini /usr/bin/runsvdir /var/service
