3172832fb78d6b22e543454b3477d5c4db780e68
Michael Aldridge <aldridge.mac@gmail.com>
2024-01-13 13:50:48 -0600

    terraform/network/peer: Implement route filters.

--- terraform/network/peer/bgp.tf
+++ terraform/network/peer/bgp.tf
@@ -18,11 +18,31 @@ resource "routeros_routing_bgp_connection" "core" {
   }
 }
 
+resource "routeros_routing_filter_rule" "mesh_export" {
+  chain   = "mesh-export"
+  rule    = "if (dst in 192.168.16.0/20) {accept} else {reject}"
+  comment = "Use Specified Exports"
+}
+
+resource "routeros_routing_filter_rule" "mesh_import_no_overlap" {
+  chain   = "mesh-import"
+  rule    = "if (dst in 192.168.16.0/20) {reject} else {accept}"
+  comment = "Do not accept overlapped prefixes"
+}
+
+resource "routeros_routing_filter_rule" "mesh_import_no_hugeprefix" {
+  chain   = "mesh-import"
+  rule    = "if (dst-len < 15) {reject} else {accept}"
+  comment = "Do not accept huge prefixes"
+}
+
 resource "routeros_routing_bgp_connection" "peer" {
-  for_each = { for k, v in var.bgp_peers : k => v if v.bgp }
+  for_each = var.bgp_peers
 
   as   = 64579
   name = each.key
+
+  disabled = !lookup(each.value, "bgp", true)
 
   connect = true
   listen  = true
@@ -38,4 +58,11 @@ resource "routeros_routing_bgp_connection" "peer" {
     address = cidrhost(each.value.addr, 0)
   }
 
+  input {
+    filter = "mesh-import"
+  }
+
+  output {
+    filter_chain = "mesh-export"
+  }
 }
