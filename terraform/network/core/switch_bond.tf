6347921e09123d81e4be150e26eb59e59b29a46e
Michael Aldridge <aldridge.mac@gmail.com>
2024-05-29 19:28:36 -0500

    terraform/network/core: Add switch bond to core2

--- terraform/network/core/switch_bond.tf
+++ terraform/network/core/switch_bond.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "routeros_interface_bonding" "bond0" {
   name    = "bond0"
-  slaves  = ["ether3", "ether4"]
+  slaves  = ["ether3", "ether6"]
   mode    = "802.3ad"
   comment = "Core Switch Link"
 }
@@ -21,4 +21,25 @@ resource "routeros_interface_bridge_vlan" "bond0" {
   vlan_ids = join(",", local.switch_vlan_ids)
   tagged   = [routeros_interface_bonding.bond0.name]
   comment  = "Core Switch Trunks"
+}
+
+resource "routeros_interface_bonding" "bond1" {
+  name    = "bond1"
+  slaves  = ["ether4", "ether7"]
+  mode    = "802.3ad"
+  comment = "PoE Switch Link"
+}
+
+resource "routeros_interface_bridge_port" "bond1" {
+  bridge    = routeros_interface_bridge.br0.name
+  interface = routeros_interface_bonding.bond1.name
+  pvid      = 1
+  comment   = "PoE Switch"
+}
+
+resource "routeros_interface_bridge_vlan" "bond1" {
+  bridge   = routeros_interface_bridge.br0.name
+  vlan_ids = join(",", local.switch_vlan_ids)
+  tagged   = [routeros_interface_bonding.bond1.name]
+  comment  = "PoE Switch Trunks"
 }
